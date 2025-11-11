2c36c0ac26f8d123670b2a8a0bf30a794dde1aa9
dias-asp <d_aspandiyarov@kbtu.kz>
2025-11-11 20:03:41 +0500

    add CI with semgrep, qodana, sonarQube

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 // Angular Material Imports
 import { MatToolbarModule } from '@angular/material/toolbar';
